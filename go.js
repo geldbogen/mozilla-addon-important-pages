8294d5b85b63f46d5afe95ce83fced13e4da41e9
Julius Niemeyer <juliusniemeyer1995@gmail.com>
2023-05-02 22:29:15 +0200

    a

--- go.js
+++ go.js
@@ -6,16 +6,37 @@ var params = {
     action: "query",
     titles: "Albert Einstein",
     prop: "langlinks",
-    format: "json"
+    format: "json",
+    lllimit: 500
 };
 
 url = url + "?origin=*";
 
 // Object.keys(params)
-fetch(url)
-.then(function(response) {
-    return response.json
-})
-.then(function(response) {
-    console.log(response)
-})
+// fetch(url)
+// .then(function(response) {
+//     return response.json
+// })
+// .then(function(response) {
+//     console.log(response)
+// })
+
+function listenForClicks() {
+    document.addEventListener("click",colorgo)
+}
+function colorgo(e) {
+    var links =document.getElementsByTagName("a");
+    for (let i = 0; i < links.length; i++) {
+        if (links[i].href) 
+        {
+            links[i].style.color="#00FF00";
+            console.log(links[i].href)
+        }       
+    }
+    
+}
+function getNumberOfLanguages(url) {
+    
+}
+
+colorgo("a");
