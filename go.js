c28cdc105f5dbec08b7591fe30de4de26bfc4b7b
Julius Niemeyer <juliusniemeyer1995@gmail.com>
2023-05-02 23:44:02 +0200

    a

--- go.js
+++ go.js
@@ -10,33 +10,51 @@ var params = {
     lllimit: 500
 };
 
+var gigastring = ``
+
+
 url = url + "?origin=*";
 
 // Object.keys(params)
-// fetch(url)
-// .then(function(response) {
-//     return response.json
-// })
-// .then(function(response) {
-//     console.log(response)
-// })
+
 
 function listenForClicks() {
-    document.addEventListener("click",colorgo)
+    document.addEventListener("click",colorgo);
 }
 function colorgo(e) {
     var links =document.getElementsByTagName("a");
-    for (let i = 0; i < links.length; i++) {
-        if (links[i].href) 
-        {
-            links[i].style.color="#00FF00";
-            console.log(links[i].href)
-        }       
-    }
+    var arr = Array.from(links);
+    const gigastring = arr.reduce((all, current) => all +"<" +current.href + "> ","");
+
+    var querystring= `prefix schema: <http://schema.org/>
+    SELECT ?url ?sitelinks WHERE {
+    VALUES ?url {`+ gigastring + `}
+    ?url schema:about ?item.
+    ?item wikibase:sitelinks ?sitelinks
+    } `
+
+    fetch(url)
+    .then(function(response) {
+    return response.json
+    })
+    .then(function(response) {
+
+    console.log(response);
     
+    })
+    .catch(function(error){console.log(error);});
+    // for (let i = 0; i < links.length; i++) {
+    //     if (links[i].href) 
+    //     {
+    //         links[i].style.color="#00FF00";
+    //         console.log(links[i].href)
+    //     }        
+    // }
+
+
 }
 function getNumberOfLanguages(url) {
-    
+
 }
 
 colorgo("a");
